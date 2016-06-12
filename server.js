a723becc61398f78c25ece6b7a60a289c6123bdc
Test A <somebodoy@test.com>
2016-06-12 17:57:20 +0100

    Server set up

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use(stylus.middleware(
     {
         src: __dirname + '/public',
@@ -27,6 +26,9 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'));
 
 
+app.get('/partials/:partialPath', function(req,res){
+    res.render('partials/' + req.params.partialPath);
+});
 app.get('*', function(req, res){
     res.render('index');
 });
